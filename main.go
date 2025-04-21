ba3056541b6f1748454e320050b77796ad08d7e1
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-04-22 00:37:19 +0800

    🎯 Implement migration command for SQL file generation

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"golang_strarter_kit_2025/app/database"
 	"golang_strarter_kit_2025/bootstrap"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	//	@securityDefinitions.apikey	X-Api-Key
-	//	@in							header
-	//	@name						X-Api-Key
+	app := &cli.App{
+		Name:  "Golang Starter Kit",
+		Usage: "CLI tool for managing migrations",
+		Commands: []*cli.Command{
+			database.MigrationCmd, // make:migration
+		},
+	}
 
-	//	@securityDefinitions.apikey	Bearer
-	//	@in							header
-	//	@name						Authorization
+	// Jalankan CLI jika ada args
+	if len(os.Args) > 1 {
+		if err := app.Run(os.Args); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
 
-	// @Security X-Api-Key
-	// @Security Bearer
-
+	// Jalankan server biasa jika tidak ada CLI arg
 	bootstrap.Init()
 }
